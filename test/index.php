33f22a355bde9475ef8ba27addb635dd2a91facb
河野太智 <taichi.kawano@supride.jp>
2018-05-23 12:31:26 +0900

    バックアッププログラム追加

--- test/index.php
+++ test/index.php
@@ -1,26 +1,47 @@
 
 <!DOCTYPE html>
 <html lang="ja">
+<link rel="stylesheet" type="text/css" href="style.css">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>代々木ヒルズ〜河野白書〜</title>
 </head>
 <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <h2>ファイル、ディレクトリの自動バックアップ設定</h2>
     <form id="form_1" method="post" accept-charset="utf-8" return false>
-        <p>ホスト名もしくはIPアドレス <input type="text" name = "userid" id ="userid"> </p>
-        <p>ユーザ名 <input type="text" name = "username" id="username"> </p>
-        <p>パスワード <input type="text" name = "passward" id="passward"> </p>
-        <p>ポート番号 <input type="text" name = "portnum" id="portnum"> </p>
-    </form>
+    <ul>
+        <li class="hostname">
+            <label for="hostname">ホスト名もしくはIPアドレス</label>
+            <input type="text" name = "hostname" id ="hostname" placeholder="バックアップ先のホスト名いれてね" size="30">
+        </li>
 
-    <form id="db_bk" method="post" accept-charset="utf-8" return false>
-        <p>DB名 <input type="text" name = "dbname" id ="dbname"> </p>
-        <p>ユーザ名 <input type="text" name = "dbusername" id="dbusername"> </p>
-        <p>パスワード <input type="text" name = "dbpassward" id="dbpassward"> </p>
-    </form>
+        <li class="username">
+            <label for="username">ユーザ名</label>
+            <input type="text" name = "username" id="username" placeholder="バックアップ先のユーザ名いれてね" size="30">
+        </li>
 
-    <button id="ajax">ajax</button>
+         <li class="passward">
+            <label for="passward">パスワード</label>
+            <input type="text" name = "passward" id="passward" placeholder="バックアップ先のパスワードいれてね" size="30">
+        </li>
+
+        <li class="portnum">
+            <label for="portnum">ポート番号 </label>
+            <input type="text" name = "portnum" id="portnum" placeholder="指定なければ未記入でいいんだ" size="30">
+        </li>
+        
+        <li class="bkpath">
+            <label for="bkpath">バックアップ先のパス </label>
+            <input type="text" name = "bkpath" id="bkpath" placeholder="指定なければ未記入でいいんだ" size="30">
+        </li>
+
+    </ul>
+    
+            <button class="square_btn"　id="ajax">これで設定すんねや</button>
+    </form>
+    
+
     <div class="result"></div>
 
     <script type="text/javascript">
@@ -32,8 +53,11 @@
                     url:'./request.php',
                     type:'POST',
                     data:{
-                        'userid':$('#userid').val(),
-                        'passward':$('#passward').val()
+                        'hostname':$('#hostname').val(),
+                        'username':$('#username').val(),
+                        'passward':$('#passward').val(),
+                        'portnum':$('#portnum').val(),
+                        'bkpath':$('#bkpath').val(),
                     }
                 })
                 // Ajaxリクエストが成功した時発動
@@ -57,6 +81,3 @@
 </body>
 </html>
 
-<?php
-$output = shell_exec("~/Documents/bashProject/projectBK.sh");
-echo $output;
--- test/request.php
+++ test/request.php
@@ -1,11 +1,26 @@
 <?php
     header('Content-type: text/plain; charset= UTF-8');
-    if(isset($_POST['userid']) && isset($_POST['passward'])){
-        $id = $_POST['userid'];
+    if(isset($_POST['hostname']) && isset($_POST['passward'])){
+        $hostname = $_POST['hostname'];
+        $username = $_POST['username'];
         $pas = $_POST['passward'];
-        $str = "AJAX REQUEST SUCCESS\nuserid:".$id."\npassward:".$pas."\n";
+        $portnum = $_POST['portnum'];
+        $bkpath = $_POST['bkpath'];
+        
+        $str = "AJAX REQUEST SUCCESS\
+        hostname:".$hostname."\n
+        username:".$username."\n
+        passward:".$pas."\n
+        portnum:".$portnum."\n
+        bkpath:".$bkpath."\n
+        ";
         $result = nl2br($str);
         echo $result;
+
+        $output = shell_exec("/var/www/html/PDP/test/bk_project.sh");
+        $output = shell_exec('/var/www/html/PDP/test/bk_project.sh "'.$hostname.'" "'.$username.'" "'.$pas.'" "'.$portnum.'" "'.$bkpath.'"');
+        echo $output;
+
     }else{
         echo 'FAIL TO AJAX REQUEST';
     }
